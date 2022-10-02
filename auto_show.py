b63b8a39fda6badab8b61204be1969ae8d60221f
Nick Hartwich <nickhartwich@gmail.com>
2022-10-01 22:35:44 -0700

    convert to tabs

--- auto_show.py
+++ auto_show.py
@@ -14,44 +14,43 @@ parser.add_argument("--show_chapter_count", type=int, help="specify chapter coun
 parser.add_argument("--show_comment_start", type=str, help="specify start of comment", default="C")
 
 def main(argv=sys.argv[1:]):
-    args = parser.parse_args(["-e", "tmp", *argv])
+	args = parser.parse_args(["-e", "tmp", *argv])
 
-    show_name = args.show_name
-    s = args.show_season
-    d = args.show_disc
+	show_name = args.show_name
+	s = args.show_season
+	d = args.show_disc
 
-    extras_base=f"{show_name}/s{s:02d}_d{d:02d}"
+	extras_base=f"{show_name}/s{s:02d}_d{d:02d}"
 
-    if not d>1:
-        ep = 1
-    else:
-        with open(f"{show_name}/s{s:02d}_d{d-1:02d}.tsv",'r') as f:
-            tsv_file = csv.reader(f, delimiter="\t")
-            for line in tsv_file:
-                ep = int(line[0][5:7])
-            ep = ep + 1
+	if not d>1:
+		ep = 1
+	else:
+		with open(f"{show_name}/s{s:02d}_d{d-1:02d}.tsv",'r') as f:
+			tsv_file = csv.reader(f, delimiter="\t")
+			for line in tsv_file:
+				ep = int(line[0][5:7])
+			ep = ep + 1
 
-    disc_info = get_disc_info(extras_base=extras_base,ProgressParser=ProgressParser,args=args)
-    print(disc_info["disc"]["name"])
-    tracks = []
-    lengths = []
-    for i, title in enumerate(disc_info["titles"]):
-        title
-        if title["chapter_count"] == args.show_chapter_count and title["comment"].startswith(args.show_comment_start): #todo: move to arguments options
-            tracks.append(i)
-            lengths.append(title["length"])
+	disc_info = get_disc_info(extras_base=extras_base,ProgressParser=ProgressParser,args=args)
+	tracks = []
+	lengths = []
+	for i, title in enumerate(disc_info["titles"]):
+		title
+		if title["chapter_count"] == args.show_chapter_count and title["comment"].startswith(args.show_comment_start): #todo: move to arguments options
+			tracks.append(i)
+			lengths.append(title["length"])
 
-    eps = range(ep,ep+len(tracks))
-    extra = f"{extras_base}.tsv"
-    with open(extra,"w") as f:
-        for i, track in enumerate(tracks):
-            f.write(f"s{s:02d}/e{eps[i]:02d}\t{lengths[i]}\n")
+	eps = range(ep,ep+len(tracks))
+	extra = f"{extras_base}.tsv"
+	with open(extra,"w") as f:
+		for i, track in enumerate(tracks):
+			f.write(f"s{s:02d}/e{eps[i]:02d}\t{lengths[i]}\n")
 
-    auto_makemkv([
-        "-e", extra,
-        "-o", show_name,
-        "--no-extra_warn",
-    ])
+	auto_makemkv([
+		"-e", extra,
+		"-o", show_name,
+		"--no-extra_warn",
+	])
 
 
 if __name__ == "__main__":
